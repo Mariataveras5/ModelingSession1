075f8f06ad8fef7e9abe79705945815e0ffcf5cd
Mariataveras5 <mariagtaveras5105@gmail.com>
2025-04-30 19:58:53 +0200

    Added docstrings

--- color_point.py
+++ color_point.py
@@ -11,6 +11,10 @@ class ColorPoint(Point):
         self.color = color
 
     def __str__(self):
+        """
+        Magic method that defines the color format of the point
+        :return: color and coordinates of the point as a string
+        """
         return f"<{self.x}, {self.y}>, ({self.color})"
 
 if __name__ == "__main__": # to make sure this code does not run when imported
